08a2fe8206e1a308ec90793db0fb05a4d103b485
louis <louis.schimetschek@hfg-gmuend.de>
2023-06-05 12:27:26 +0200

    tiles and filter

--- src/lib/entries/tiles.js
+++ src/lib/entries/tiles.js
@@ -31,4 +31,40 @@ export default [
                 author: 'Louis Schimetschek',
                 tags: ["Internet", "Browser", "Software"]
         },
+        {
+                title: 'Library jQuery',
+                folder: 'jQuery',
+                description:
+                        'jQuery ist eine umfangreiche JavaScript-Bibliothek, die die Webentwicklung durch DOM-Manipulation, Event-Handling, Animationen, Effekte und AJAX erleichtert und dadurch ein dynamisches Benutzererlebnis ermöglicht.',
+                tags: ['JavaScript-Bibliothek', 'Event-Handling', 'Interaktive Webanwendungen'],
+                author: 'Anja Gutmann',
+                github: 'https://github.com/anjagutmann/jQuery.git',
+        },
+        {
+                title: 'Repository',
+                folder: 'repository',
+                description: 'Ein Repository ist eine zentrale Ablage in dem digitale Daten verwaltet werden.',
+                tags: ['Versionskontrolle', 'Software Development', 'Collaboration', 'Data'],
+                author: 'Anna Ligon',
+                github: 'https://github.com/annxligon/repository.git',
+        },
+        {
+                title: 'URL',
+                folder: 'Url_Svelte',
+                description: 'URL steht für Uniform Resource Locator und ist eine Zeichenkette, die verwendet wird, um eine bestimmte Ressource im Internet zu identifizieren.',
+                tags: ['Internet', 'Webadresse', 'HTML'],
+                author: 'Nina Hielscher',
+                github: 'https://github.com/NinaHielscher/URL.git',
+        },
+        {
+                title: 'Margin & Padding',
+                folder: 'margin_padding',
+                description: 'Mit Margin und Padding kann man festlegen wo welche Elemente auf der Homepage mit welchem Abstand sind.',
+                tags: ['Layout', 'HTML', 'CSS', 'Webdesign'],
+                author: 'Nina Hielscher',
+                github: 'https://github.com/NinaHielscher/URL.git',
+        },
+        
+
+
 ];
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,166 @@
 <script>
-        import '../app.css';
+        import tiles from '$lib/entries/tiles.js';
+
+        let filteredTiles = tiles;
+        let tagList = [];
+        let filtersActive = false;
+
+        //einmal alle verfügbaren tags durchgehen und liste erstellen in der jedes tag nur einmal vorkommt
+        tiles.forEach((tile) => {
+                tile.tags.forEach((tag) => {
+                        //duplikate suchen
+                        let duplicate = false;
+
+                        tagList.forEach((tagObject) => {
+                                if (tagObject.name == tag) {
+                                        console.log('doppelt, nicht rein');
+                                        duplicate = true;
+                                        //wir brauchen nicht weiter checken
+                                        //das return springt aus der forEach-schleife
+                                        return;
+                                }
+                        });
+
+                        //wenn nach allen checks ein duplikat ausgeschlossen wurde dann hinzufügen
+                        if (!duplicate) {
+                                tagList.push({ name: tag, active: false });
+                        }
+                });
+        });
+
+        let toggleFilter = (i) => {
+                tagList[i].active = !tagList[i].active;
+        };
+
+        //wenn sich filter ändern dann nimm alle tiles die ein tag besitzen, dass in der taglist active==true ist mit in die filtered tiles
+        $: {
+                filteredTiles = [];
+                let activeFilters = 0;
+                tagList.forEach((tag) => {
+                        if (tag.active) {
+                                activeFilters++;
+                                filtersActive = true;
+                                console.log('activetag!');
+                                tiles.forEach((tile) => {
+                                        tile.tags.forEach((tagToCheck) => {
+                                                if (tagToCheck == tag.name) {
+                                                        console.log('success: ' + tagToCheck + '==' + tag.name);
+                                                        if (!filteredTiles.includes(tile)) {
+                                                                filteredTiles.push(tile);
+                                                                filteredTiles = filteredTiles;
+                                                                console.log('gibts noch nicht, also rein da');
+                                                        }
+                                                }
+                                        });
+                                });
+                        }
+                        console.log('Neue änderung, neue Tiles: ', filteredTiles);
+                        filtersActive = activeFilters > 0 ? true : false;
+                });
+        }
 </script>
 
-<header>
-        <a href="/">
-                <h1>Prog3 Lexikon</h1>
-        </a>
-        <nav>
-                <ul>
-                        <li><a href="#">About Me</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Portfolio</a></li>
-                </ul>
-        </nav>
-</header>
+<div class="container">
+        <h1>Glossar</h1>
+        <div class="tags">
+                {#each tagList as filter, i}
+                        <button class="tag filter" on:click={() => toggleFilter(i)} class:active={filter.active}>
+                                {filter.name}
+                        </button>
+                {/each}
+        </div>
 
-<slot />
+        <div class="tile-grid">
+                {#if filtersActive}
+                        {#each filteredTiles as tile}
+                                <a href={'details/' + tile.folder}>
+                                        <div class="tile">
+                                                <h3>{tile.title}</h3>
+                                                <p class="tile-content">{tile.description}</p>
+                                                <div class="tags">
+                                                        {#each tile.tags as tag}
+                                                                <div class="tag">{tag}</div>
+                                                        {/each}
+                                                </div>
+                                        </div>
+                                </a>
+                        {/each}
+                {:else}
+                        {#each tiles as tile}
+                                <a href={'details/' + tile.folder}>
+                                        <div class="tile">
+                                                <h3>{tile.title}</h3>
+                                                <p class="tile-content">{tile.description}</p>
+                                                <div class="tags">
+                                                        {#each tile.tags as tag}
+                                                                <div class="tag">{tag}</div>
+                                                        {/each}
+                                                </div>
+                                        </div>
+                                </a>
+                        {/each}
+                {/if}
+        </div>
+</div>
 
 <style>
-        nav {
-                background-color: rgb(39, 75, 107);
-                border-radius: 4px;
-                padding-left: 20px;
+        .filter:hover {
+                color: white;
+                background-color: grey;
         }
-        a {
+
+        .active {
+                background-color: rgb(73, 136, 192);
                 color: white;
         }
 
-        nav ul {
-                list-style: none;
-                padding: 0;
-                height: 32px;
+        .tags {
+                display: flex;
+                width: 100%;
+                flex-wrap: wrap;
         }
-
-        nav ul li {
-                display: inline;
-                height: 100%;
-                justify-content: center;
-                padding-right: 1em;
+        .tag {
+                display: flex;
+                background-color: none;
+                padding: 2px 8px;
+                border-radius: 8px;
+                margin: 4px 8px;
+                border: 1px solid grey;
         }
-
-        header {
-                height: 100px;
+        .container {
                 padding: 20px;
-                background-color: rgb(27, 60, 88);
-                color: white;
+                min-height: 85vh;
         }
 
-        header h1 {
+        .tile-grid {
+                display: grid;
+                gap: 10px;
+                grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+        }
+
+        .tile {
+                border-radius: 0.5em;
+                color: black;
+                background-color: rgb(226, 226, 226);
+                position: relative;
+                transition: all 0.1s ease-in;
+                cursor: pointer;
+        }
+
+        .tile:hover {
+                background-color: rgb(177, 201, 231);
+        }
+
+        .tile-content {
+                padding: 1em;
+        }
+
+        .tile h3 {
+                color: black;
+                background-color: rgb(187, 187, 187);
+                border-radius: 0.5em 0.5em 0 0;
                 margin: 0;
+                line-height: 50px;
+                padding: 0 1em;
         }
 </style>
